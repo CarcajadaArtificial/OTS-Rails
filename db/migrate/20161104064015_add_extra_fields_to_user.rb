1fae9ce149f0e4ed075509c43fc9412007d46afe
Daniel Gonzalez <daniel.gv.1993@gmail.com>
2016-11-07 20:16:05 -0600

    cambie Users a users en el migrate

--- db/migrate/20161104064015_add_extra_fields_to_user.rb
+++ db/migrate/20161104064015_add_extra_fields_to_user.rb
@@ -1,5 +1,5 @@
 class AddExtraFieldsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :Users, :AdminStatus, :boolean, default: false
+    add_column :users, :AdminStatus, :boolean, default: false
   end
 end
