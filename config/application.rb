4689ffb055696a21ce9b7829a0d5989edc31cc6b
Daniel Gonzalez <daniel.gv.1993@gmail.com>
2017-01-26 21:45:30 -0600

    agregué config.serve_static_files = true a application.rb 2

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module OTSApplication
   class Application < Rails::Application
+    config.serve_static_files = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-config.serve_static_files = true
